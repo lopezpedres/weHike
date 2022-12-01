20d3fbb2b719b0163913f1d95ba6c89ee34f7a69
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-11-30 20:02:23 -0700

    Started firebase Queries types

---
+++ src/firebase/firebaseQueries/firebaseQueries.d.ts
@@ -0,0 +1,15 @@
+import { FieldValue } from "firebase/firestore";
+//Add new user
+export default interface InterfaceNewTrailArg {
+  trail_id: string;
+  trail_name: string;
+  tags?: string[];
+}
+export interface InterfaceNewUserTrail {
+  [key: string]: {
+    trail_name: string;
+    tags?: string[];
+    createdAt: FieldValue;
+    updatedAt: FieldValue;
+  };
+}
