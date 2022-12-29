797ca56cdaffd399e9af4551b47d0884abd90e86
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-12-28 17:40:40 -0800

    All works but need to move modal

--- src/components/ImagesCollection/ImagesCollection.tsx
+++ src/components/ImagesCollection/ImagesCollection.tsx
@@ -1,25 +1,47 @@
-import { ImagesTrail } from "../MyTrailMap/typesMyTrailMap";
+import { useState } from "react";
+import { ImagesTrail, imageTrail } from "../MyTrailMap/typesMyTrailMap";
 
 interface Props {
   trailImages: ImagesTrail;
 }
 const ImagesCollection = ({ trailImages }: Props) => {
+  const [showImage, setShowImage] = useState(false);
+  const [displayedImageDetails, setDisplayedImageDetails] = useState<
+    imageTrail | undefined
+  >();
+  const onClickHandler = (value: imageTrail) => {
+    setShowImage(true);
+    setDisplayedImageDetails(value);
+  };
   return (
-    <section className="p-7">
-      <h5 className="text-xl font-semibold pb-4">Media</h5>
-      <ul className="grid grid-cols-3 gap-2">
-        {Object.entries(trailImages)
-          //Need to sort this by uploading date
-          .sort()
-          .map(([key, value]) => (
-            <li key={key}>
-              <img
-                className="object-cover w-full h-full"
-                src={value.image_url}
-              />
-            </li>
-          ))}
-      </ul>
+    <section>
+      <h5 className="px-7 text-xl font-semibold pb-4">Media</h5>
+      {trailImages && (
+        <ul className="grid grid-cols-3 gap-2 px-7">
+          {Object.entries(trailImages)
+            //TODO: Need to sort this by uploading date
+            .sort()
+            .map(([key, value]) => (
+              <li key={key}>
+                <img
+                  onClick={() => onClickHandler(value)}
+                  className="object-cover w-full h-full"
+                  src={value.image_url}
+                />
+              </li>
+            ))}
+        </ul>
+      )}
+      {!trailImages && <h1>Loading</h1>}
+      {showImage && (
+        <article className="absolute  top-1/2 -translate-y-1/2 overflow-y-hidden">
+          <img
+            onClick={() => setShowImage(false)}
+            src={displayedImageDetails?.image_url}
+            className=""
+          />
+        </article>
+      )}
     </section>
   );
 };
